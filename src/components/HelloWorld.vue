92b9fc31c5c55443ea8e0e82b96b0ccbea1ecdf1
Johan Boberg <boberj@users.noreply.github.com>
2022-01-22 22:29:28 +0100

    Proof of concept

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <ul>
+    <li><input v-model="username" placeholder="Username" /></li>
+    <li><input v-model="token" type="password" placeholder="Token"/></li>
+    <li><button v-on:click="load">Load data</button></li>
+  </ul>
+  <ul>
+    <li v-for="item in work" :key="item.id">
+      ({{ item.state }})
+      <a :href="item.url">{{ item.title }}</a>
+      ({{ item.author }})
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      username: "",
+      token: "",
+      response: {},
+      work: [],
+    };
+  },
+  methods: {
+    load() {
+      const baseUrl = "https://bitbucket.navico.com/rest/api/1.0";
+      const dashboard = "/dashboard/pull-requests";
+      const url = baseUrl + dashboard;
+      const init = {
+        headers: new Headers({
+          Authorization: "Basic " + btoa(`${this.username}:${this.token}`),
+        }),
+        credentials: "include",
+      };
+
+      fetch(url, init)
+        .then((r) => r.json())
+        .then((r) => {
+          this.response = r;
+
+          this.work = r.values.reduce((acc, item) => {
+            acc.push({
+              id: item.id,
+              state: item.state,
+              title: item.title,
+              author: item.author.user.displayName,
+              url: item.links.self[0].href,
+            });
+            return acc;
+          }, []);
+        });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
